47121dc640891ebd1bb5c4e74757c57b36a528f7
TTVErraticAlcoholic <lerchm10@gmail.com>
2020-12-21 19:41:11 +0100

    Add project files.

---
+++ qr creator/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace qr_creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 303);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(130, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate QR code";
+            this.toolTip1.SetToolTip(this.button1, "Generates a QR code with the value specified below");
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 208);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.toolTip1.SetToolTip(this.pictureBox1, "Click me to save as a .png!");
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::qr_creator.Properties.Resources.cancel_white;
+            this.pictureBox2.Location = new System.Drawing.Point(351, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.toolTip1.SetToolTip(this.pictureBox2, "Closes the QR Creator");
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(12, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 20);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Open save folder";
+            this.toolTip1.SetToolTip(this.button2, "Opens the folder the QR codes are stored in");
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(263, 278);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Random QR color";
+            this.toolTip1.SetToolTip(this.checkBox1, "Generates QR codes with a random color");
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(263, 255);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(77, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Keep input";
+            this.toolTip1.SetToolTip(this.checkBox2, "If this is checked, the textbox will not clear after the QR code has been generat" +
+        "ed");
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Your QR code will appear here!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(387, 337);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR Creator by Erratic";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ qr creator/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using QRCoder;
+using System.Diagnostics;
+using System.IO;
+using AutoUpdaterDotNET;
+
+
+namespace qr_creator
+{
+    public partial class Form1 : Form
+    {
+
+        public static string saveFolder = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "GeneratedQRCodes");
+        public static string fullFile;
+        public static string FrontQRColor;
+        public static string BackQRColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            AutoUpdater.Start("https://raw.githubusercontent.com/TTVErraticAlcoholic/QR_Creator/main/version.xml");
+
+            if (!Directory.Exists(saveFolder))
+            {
+                Directory.CreateDirectory(saveFolder);
+            }
+        }
+
+        private bool mouseDown;
+        private Point lastLocation;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isUri = Uri.IsWellFormedUriString(textBox1.Text, UriKind.RelativeOrAbsolute);
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please enter a valid value below first", "Attention!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                label1.Text = "Successfully generated QR code!";
+                if (!isUri)
+                {
+                    if (!checkBox1.Checked)
+                    {
+                        FrontQRColor = "#4287f5";
+                        BackQRColor = "#000000";
+                    }
+                    else
+                    {
+                        var random = new Random();
+                        FrontQRColor = $"#{random.Next(0x1000000):X6}";
+                        BackQRColor = $"#{random.Next(0x1000000):X6}";
+                    }
+                    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.Q);
+                    QRCode qrCode = new QRCode(qrCodeData);
+                    Bitmap qrCodeImage = qrCode.GetGraphic(20, FrontQRColor, BackQRColor);
+                    pictureBox1.Image = qrCodeImage;
+                    toolTip1.SetToolTip(pictureBox1, "Click to save: " + textBox1.Text);
+                    label1.ForeColor = Color.LightGreen;
+                    label2.Hide();
+                    if (!checkBox2.Checked)
+                    {
+                        textBox1.Text = "";
+                    }
+
+                    var t = new Timer();
+                    t.Interval = 2000;
+                    t.Tick += (s, g) =>
+                    {
+                        label1.Hide();
+                        t.Stop();
+                    };
+                    t.Start();
+                }
+                else
+                {
+                    if (!checkBox1.Checked)
+                    {
+                        FrontQRColor = "#4287f5";
+                        BackQRColor = "#000000";
+                    }
+                    else
+                    {
+                        var random = new Random();
+                        FrontQRColor = $"#{random.Next(0x1000000):X6}";
+                        BackQRColor = $"#{random.Next(0x1000000):X6}";
+                    }
+                    PayloadGenerator.Url generator = new PayloadGenerator.Url(textBox1.Text);
+                    string payload = generator.ToString();
+
+                    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+                    QRCode qrCode = new QRCode(qrCodeData);
+                    var qrCodeAsBitmap = qrCode.GetGraphic(20, FrontQRColor, BackQRColor);
+                    pictureBox1.Image = qrCodeAsBitmap;
+                    toolTip1.SetToolTip(pictureBox1, "Click to save: " + textBox1.Text);
+                    label1.ForeColor = Color.LightGreen;
+                    label2.Hide();
+                    if (!checkBox2.Checked)
+                    {
+                        textBox1.Text = "";
+                    }
+
+                    var t = new Timer();
+                    t.Interval = 2000;
+                    t.Tick += (s, g) =>
+                    {
+                        label1.Text = "";
+                        t.Stop();
+                    };
+                    t.Start();
+                }
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Try generating a QR code first", "Attention!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (!Directory.Exists(saveFolder))
+                {
+                    Directory.CreateDirectory(saveFolder);
+                }
+                string file = RandomFileName();
+                fullFile = saveFolder + @"\" + file + ".png";
+                pictureBox1.Image.Save(fullFile, ImageFormat.Png);
+                label1.Text = "   Successfully saved QR code!";
+
+                var t = new Timer();
+                t.Interval = 2000;
+                t.Tick += (s, g) =>
+                {
+                    label1.Text = "";
+                    t.Stop();
+                };
+                t.Start();
+            }           
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            var random = new Random();
+            return random.Next(min, max);
+        }
+        public string RandomString(int size, bool lowerCase = false)
+        {
+            var random = new Random();
+            var builder = new StringBuilder(size);
+
+            // Unicode/ASCII Letters are divided into two blocks
+            // (Letters 65–90 / 97–122):
+            // The first group containing the uppercase letters and
+            // the second group containing the lowercase.  
+
+            // char is a single Unicode character  
+            char offset = lowerCase ? 'a' : 'A';
+            const int lettersOffset = 26; // A...Z or a..z: length=26  
+
+            for (var i = 0; i < size; i++)
+            {
+                var @char = (char)random.Next(offset, offset + lettersOffset);
+                builder.Append(@char);
+            }
+
+            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+        }
+
+        public string RandomFileName()
+        {
+            var passwordBuilder = new StringBuilder();
+
+            // 4-Letters lower case   
+            passwordBuilder.Append(RandomString(4, true));
+
+            // 4-Digits between 1000 and 9999  
+            passwordBuilder.Append(RandomNumber(1000, 9999));
+
+            // 2-Letters upper case  
+            passwordBuilder.Append(RandomString(2));
+            return passwordBuilder.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(saveFolder);
+            }
+            catch (Win32Exception)
+            {
+                MessageBox.Show("Save folder not found. The fuck have you done?", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
